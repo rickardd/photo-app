f3be354abc2a43b3b44f9a22b9892410463b4388
Rickard Dahlstrom <dahlstrom.rickard@gmail.com>
2016-08-04 01:21:25 +0200

    Added Stripe ... and testing sublime-git commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # meaning... go to my registrations controller and then to devise registrations controller
+  devise_for :users, controllers: { :registrations => "registrations" }
   root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
